4485c8e49dff3ac4d1efd4529d2c37036db95bf8
leesiyoung <tldud4861@gmail.com>
2022-07-28 16:20:37 +0900

    q3

--- Q3.py
+++ Q3.py
@@ -2,8 +2,8 @@
 
 def solution(left, right):
     answer = 0
-    count=0
     for i in range(left, right+1):
+        count=0
         for j in range(1, right+1):
             if i%j==0:
                 count+=1
@@ -11,7 +11,6 @@ def solution(left, right):
             answer+=i
         else:
             answer-=i
-        count=0
     return answer
 
 left = 13
